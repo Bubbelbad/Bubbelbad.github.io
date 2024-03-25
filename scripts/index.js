188a91254eb48a6addcfd642587e742bb4f80b7a
Bubbelbad <victor.ivarson@hotmail.se>
2024-03-25 22:10:41 +0100

    new banner-background on index (need adjustments still)

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+    const button = document.getElemendById("button");
+
+    button.addEventListener("click", function(event)
+    {
+        function(event)
+        {
+            thirdText.text-decoration
+        }
+    }
