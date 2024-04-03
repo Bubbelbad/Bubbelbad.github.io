76a274daf1bd3e72d2e66a39088832ff61eda413
Bubbelbad <victor.ivarson@hotmail.se>
2024-04-03 15:06:51 +0200

    Added top-nav, text and animations for portfolio-grid, changed banner-picture + some more

---
+++ scripts/grid-portfolio.js
@@ -0,0 +1,24 @@
+const grid = document.getElementById('grid-portfolio');
+
+
+grid.addEventListener('mouseenter', 
+    function(event) {
+        section1.style.backgroundColor = 'red';
+    }
+);
+
+const galleryText = document.getElementById('galleryText');
+
+something.addEventListener('mouseEnter',
+    function(event) {
+        something.style.backgroundColor = 'blue';
+    }
+);
+
+const something = document.getElementById('galleryText');
+
+something.addEventListener('mouseEnter',
+    function(event) {
+        something.style.backgroundColor = 'blue';
+    }
+);
