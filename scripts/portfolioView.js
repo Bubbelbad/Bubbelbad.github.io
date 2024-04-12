a364e6f9c72e8d27219f32a7bd649d7e34ba1560
Bubbelbad <victor.ivarson@hotmail.se>
2024-04-12 12:11:21 +0200

    Updated first section of portfilioView with new variables and links

--- scripts/portfolioView.js
+++ scripts/portfolioView.js
@@ -4,20 +4,23 @@
 
 let projectList = [];
 let projectName1 = document.getElementById("project-name");
+let projectMonth1 = document.getElementById("project-month");
 let projectDescription1 = document.getElementById("project-description");
+let projectTags1 = document.getElementById("project-tags");
+let projectLink1 = document.getElementById("project-link");
 
 
-let electricCars = new Project("ElectricCars", "The purpose of this application was to create an app with 5 design patterns and two sorting algorithms. I chose to make a fictive database that contains data for electric vehicles. <br/> The program displays a list of car proxies and the user is able to sort the proxies by brand or price, using two sorting algorithms.", ["WPF", "Design Patterns"]);
+let electricCars = new Project("ElectricCars", "MARS 2024", "The purpose of this application was to create an app with 5 design patterns and two sorting algorithms. I chose to make a fictive database that contains data for electric vehicles. <br/> The program displays a list of car proxies and the user is able to sort the proxies by brand or price, using two sorting algorithms.", ["WPF", "Design Patterns, Sorting Algorithms, C#"], "https://github.com/Bubbelbad/ElectricCars");
 
-let miniLibrary = new Project("MiniLibrary", "This is a small version of a library. The goal of this project is to practice MySQL and database connection to the C# application. Also to give different privileges and growing my understanding on views, procedures etc.", ["MySQL", "WPF"]);
+let miniLibrary = new Project("MiniLibrary", "FEBRUARY 2024", "This is a small version of a library. The goal of this project is to practice MySQL and database connection to the C# application. Also to give different privileges and growing my understanding on views, procedures etc.", ["MySQL", "WPF"], "https://github.com/Bubbelbad/MiniLibrary");
 
-let noteTakingApp = new Project("NoteTakingApp", "This is a school assignment for my course 'object oriented programming I' in Windows Forms. I chose this project because I wanted to practice file-handling and saving things localy. It should be a very simple design that allows you to create messages and save author, title, category and if it's a secret. ", ["Windows Forms", "File Handling"]);
+let noteTakingApp = new Project("NoteTakingApp", "NOVEMBER 2023", "This is a school assignment for my course 'object oriented programming I' in Windows Forms. I chose this project because I wanted to practice file-handling and saving things localy. It should be a very simple design that allows you to create messages and save author, title, category and if it's a secret. ", ["Windows Forms", "File Handling"], "https://github.com/Bubbelbad/TheNoteTakingApp-WindowsForms");
 
-let filmLibrary = new Project("FilmLibrary", "This application should let the user browse movies for inspiration. The user should be able to save the movies to different lists, like 'favorites' and 'watch later'.", ["MySQL", "WPF"]);
+let filmLibrary = new Project("FilmLibrary", "DECEMBER 2023", "This application should let the user browse movies for inspiration. The user should be able to save the movies to different lists, like 'favorites' and 'watch later'.", ["MySQL", "WPF"], "https://github.com/Bubbelbad/FilmLibrary");
 
-let battleShip = new Project("BattleShip", "My goal is to make an simplified single player version of this game, with only one game field with four hidden boats. The goal is to uncover all hidden boats in as few moves as possible.", ["Console App", "Game"]);
+let battleShip = new Project("BattleShip", "NOVEMBER 2023", "My goal is to make an simplified single player version of this game, with only one game field with four hidden boats. The goal is to uncover all hidden boats in as few moves as possible.", ["Console App", "Game"], "https://github.com/Bubbelbad/Battleship");
 
-let theFarm = new Project("TheFarm", "Uppgiften gick it på att utveckla ett program från utifrån redan existerande Klass-Diagram. Klasserna skickar olika klasser emellan sig och blir ett trevligt litet eco-system", ["Console App"]);
+let theFarm = new Project("TheFarm", "SEPTEMBER 2023", "Uppgiften gick it på att utveckla ett program från utifrån redan existerande Klass-Diagram. Klasserna skickar olika klasser emellan sig och blir ett trevligt litet eco-system", ["Console App"], "https://github.com/Bubbelbad/TheFarm-ConsoleApp");
 
 projectList.push(electricCars);
 projectList.push(miniLibrary);
@@ -33,11 +36,13 @@ let project = null;
 for (i = 0; i < projectList.length; i++) {
     if (projectList[i].getName() == projectTitle)  
         project = projectList[i];
-}
+};
 
-console.log(projectName1);
 projectName1.textContent = project.getName();
+projectMonth1.textContent = project.getMonth();
 projectDescription1.innerHTML = project.getDescription();
+projectTags1.textContent = project.getTagList();
+projectLink1.href = project.getProjectLink();
 
 
 
--- scripts/project.js
+++ scripts/project.js
@@ -1,17 +1,37 @@
 class Project {
 
-    constructor(name, description) {
+    constructor(name, month, description, tagList, projectLink) {
         this.name = name;
+        this.month = month;
         this.description = description;
-        this.tagList = [];
+        this.tagList = tagList;
+        this.projectLink = projectLink;
     }
 
     getName() {
         return this.name;
     }
 
+    getMonth() {
+        return this.month;
+    }   
+
     getDescription() {
         return this.description;
+    }
+
+    getTagList() {
+        let string = "";
+        for (let i = 0; i < this.tagList.length; i++) {
+            if (i == this.tagList.length - 1) {
+                return string + this.tagList[i];
+            }
+            else string += this.tagList[i] + ", ";
+        }
+    }
+
+    getProjectLink() {
+        return this.projectLink;
     }
 
     setName(name) {
@@ -20,6 +40,10 @@ class Project {
 
     setDescription(description) {
         this.description = description;
+    }
+
+    setTagList(tagList) {
+        this.tagList = tagList;
     }
 
     static fromJson(json) {
